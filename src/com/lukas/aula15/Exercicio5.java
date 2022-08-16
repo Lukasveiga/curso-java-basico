14cd05369675bf03db11680d4133ff57ec8e20ff
Lukas Veiga <lukas.veiga@hotmail.com>
2022-08-15 21:58:33 -0300

    new exercises

---
+++ src/com/lukas/aula15/Exercicio5.java
@@ -0,0 +1,26 @@
+package com.lukas.aula15;
+
+public class Exercicio5 {
+
+    public static void main(String[] args) {
+
+        double nota1 = 8.7;
+        double nota2 = 6.9;
+        resultadoFinal(nota1, nota2);
+        
+    }
+
+    public static void resultadoFinal(double nota1, double nota2) {
+
+        double media = (nota1 + nota2)/2;
+
+        if (media >= 7){
+            System.out.println("Aprovado!");
+        } else if (media < 7) {
+            System.out.println("Reprovado.");
+        } else if (media == 10) {
+            System.out.println("Aprovado com distinção, parabéns!");
+        }
+    }
+    
+}
