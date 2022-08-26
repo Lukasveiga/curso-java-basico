9a6284e6e3ae86ca20c507bfe0b4bb15b3ce6c70
Lukas Veiga <lukas.veiga@hotmail.com>
2022-08-26 09:40:15 -0300

    new class

---
+++ src/com/lukas/aula28/Pacotes.java
@@ -0,0 +1,18 @@
+package com.lukas.aula28;
+
+import com.lukas.aula27.Carro;
+import com.lukas.aula27.JogoDaVelha;
+
+public class Pacotes {
+
+    public static void main(String[] args) {
+
+        Carro carro = new Carro();
+
+        JogoDaVelha jogoTeste = new JogoDaVelha();
+        
+        
+        
+    }
+    
+}
