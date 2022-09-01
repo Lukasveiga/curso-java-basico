bd7db669fe1c01b2b9bc626e9782f8dd07774b52
Lukas Veiga <lukas.veiga@hotmail.com>
2022-09-01 11:29:30 -0300

    update exercise

--- src/com/lukas/aula43/exercicios/ex02/App.java
+++ src/com/lukas/aula43/exercicios/ex02/App.java
@@ -4,28 +4,29 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("----- Exemplo Pessoa Jurídica ------");
         Contribuinte pessoaJuridica = new PessoaJuridica();
         pessoaJuridica.setNome("Java Estudos LTDA");
         pessoaJuridica.setRenda(15000);
-
         pessoaJuridica.calcularImposto();
-        System.out.println(pessoaJuridica);
 
-        System.out.println("----- Exemplo 1 Pessoa Física ------");
         Contribuinte pessoaFisica = new PessoaFisica();
         pessoaFisica.setNome("Lukas");
         pessoaFisica.setRenda(1500);
-
         pessoaFisica.calcularImposto();
-        System.out.println(pessoaFisica);
 
-        System.out.println("----- Exemplo 2 Pessoa Física ------");
         Contribuinte pessoaFisica2 = new PessoaFisica();
         pessoaFisica2.setNome("Marcos");
         pessoaFisica2.setRenda(3800);
-
         pessoaFisica2.calcularImposto();
-        System.out.println(pessoaFisica2);
+
+        Contribuinte[] contribuintes = new Contribuinte[3];
+        contribuintes[0] = pessoaJuridica;
+        contribuintes[1] = pessoaFisica;
+        contribuintes[2] = pessoaFisica2;
+
+        for (Contribuinte contribuinte: contribuintes) {
+            System.out.println(contribuinte);
+            System.out.println("-------------------------------------");
+        }
     }
 }
