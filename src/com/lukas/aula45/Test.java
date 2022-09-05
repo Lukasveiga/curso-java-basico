aabfd06787b7806484b1018fb3276b41f5b192d5
Lukas Veiga <lukas.veiga@hotmail.com>
2022-09-05 09:29:34 -0300

    new class

---
+++ src/com/lukas/aula45/Test.java
@@ -0,0 +1,19 @@
+package com.lukas.aula45;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        //upcasting
+        //Aluno aluno = new Aluno();
+        //Pessoa pessoaAluno = aluno; 
+//
+        //Pessoa aluno2 = (Pessoa) new Aluno();
+//
+        //Pessoa aluno3 = new Pessoa();
+        //Aluno aluno4 = (Aluno) aluno3;
+        //
+        
+    }
+    
+}
---
+++ src/com/lukas/aula45/Test2.java
@@ -0,0 +1,19 @@
+package com.lukas.aula45;
+
+public class Test2 {
+    
+    public static void main(String[] args) {
+
+        //Object obj1 = obterString();
+        //String s1 = (String) obj1;
+
+        //Object obj2 = "Minha String";
+        //String s2 = (String) obj2;
+
+        
+    }
+
+    public static String obterString() {
+        return "minha String";
+    }
+}
