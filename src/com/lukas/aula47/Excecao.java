45f340e5975942dd10a10ba80389aaa906379d27
Lukas Veiga <lukas.veiga@hotmail.com>
2022-11-05 15:25:17 -0300

    update

--- src/com/lukas/aula47/Excecao.java
+++ src/com/lukas/aula47/Excecao.java
@@ -9,9 +9,12 @@ public class Excecao {
             System.out.println("Antes da exception.");
             vetor[4] = 1;
             System.out.println("Esse texto não será impresso.");
-        } catch (ArrayIndexOutOfBoundsException exception) {
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.toString());
             System.out.println("Erro ao acessar um índice do vetor que não existe.");
         }
         System.out.println("Esse texto será impresso após a exception.");
     }
 }
+
+// Always catch the most specific exception class first and add the less specific catch blocks to the end of your list.
